cf7a23e8694e8142f3c13a8698060c6c1153bff5
Abdallahhany <abdallah.hanaa7706@ci.menofia.edu.eg>
2021-09-30 17:36:24 +0200

    fix package.json

--- backend/app.js
+++ backend/app.js
@@ -37,6 +37,7 @@ app.use('/api/v1', payment);
 if (process.env.NODE_ENV === 'PRODUCTION') {
     app.use(express.static(path.join(__dirname, '../frontend/build')))
 
+
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
     })
