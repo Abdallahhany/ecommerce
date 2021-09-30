1681c41c46552700c3b410951a4f7e393ca281a2
Abdallahhany <abdallah.hanaa7706@ci.menofia.edu.eg>
2021-09-30 17:29:12 +0200

    fix app

--- backend/app.js
+++ backend/app.js
@@ -33,13 +33,15 @@ app.use('/api/v1', auth);
 app.use('/api/v1', order);
 app.use('/api/v1', payment);
 
-if(process.env.NODE_ENV === 'PRODUCTION'){
-    app.use(express.static(path.join(__dirname,'../frontend/build')));
 
-    app.get('*',(req, res) => {
-        res.sendFile(path.resolve(__dirname,'../frontend/build/index.html'))
+if (process.env.NODE_ENV === 'PRODUCTION') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
     })
 }
+
 
 
 // Middleware to error
